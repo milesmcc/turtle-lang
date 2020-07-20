9d65e5df156d185ee338a737a5e80dedc5ac705c
R. Miles McCain <development-machine@sendmiles.email>
2020-07-20 08:19:44 +0000

    Implement exception formatting

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,4 +1,7 @@
-use crate::{CallSnapshot, Symbol};
+use crate::{parser, CallSnapshot, Keyword, SourcePosition, Symbol};
+use ansi_term::{Color, Style};
+use std::error::Error;
+use std::fmt;
 use std::sync::{Arc, RwLock};
 
 #[macro_export]
@@ -29,12 +32,44 @@ pub enum ExceptionValue {
     Other(String),
     UndefinedSymbol(Symbol),
     ArgumentMismatch,
+    Syntax,
+}
+
+impl ExceptionValue {
+    pub fn explain(&self) -> String {
+        use ExceptionValue::*;
+
+        match self {
+            Other(val) => val.clone(),
+            UndefinedSymbol(symbol) => format!("the symbol `{}` has no assigned value", symbol),
+            ArgumentMismatch => format!("the arguments to this function are invalid"),
+            Syntax => format!("the syntax of this code is incorrect"),
+        }
+    }
+
+    pub fn keyword(&self) -> Keyword {
+        use ExceptionValue::*;
+
+        Keyword::from_str(match self {
+            Other(_) => "other-exp",
+            UndefinedSymbol(_) => "undefined-symbol-exp",
+            ArgumentMismatch => "argument-mismatch-exp",
+            Syntax => "syntax-exp",
+        })
+    }
+}
+
+impl fmt::Display for ExceptionValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({})", self.explain(), self.keyword())
+    }
 }
 
 #[derive(Clone)]
 pub struct Exception<'a> {
     value: ExceptionValue,
     snapshot: Option<Arc<RwLock<CallSnapshot<'a>>>>,
+    additional_sources: Vec<SourcePosition>,
     note: Option<String>,
 }
 
@@ -48,6 +83,72 @@ impl<'a> Exception<'a> {
             value,
             snapshot,
             note,
+            additional_sources: vec![],
+        }
+    }
+}
+
+impl From<pest::error::Error<parser::Rule>> for Exception<'_> {
+    fn from(err: pest::error::Error<parser::Rule>) -> Self {
+        use pest::error::InputLocation::*;
+
+        let (start, end) = match err.location {
+            Pos(start) => (start, start),
+            Span((start, end)) => (start, end),
+        };
+
+        Self {
+            value: ExceptionValue::Syntax,
+            snapshot: None,
+            note: Some(format!("{}", err)),
+            // TODO: find a nice way to extract the text-level information
+            additional_sources: vec![],
+        }
+    }
+}
+
+impl fmt::Display for Exception<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{}{} {}",
+            Color::Red.bold().paint("error"),
+            Style::new()
+                .bold()
+                .paint(format!(": {}", self.value.explain())),
+            Color::Yellow.paint(format!("{}", self.value.keyword()))
+        );
+
+        match &self.snapshot {
+            Some(snapshot_lock) => match snapshot_lock.read() {
+                Ok(snapshot) => match snapshot.expression().source() {
+                    Some(source) => {
+                        // TODO: print the snapshot instead (include call stack)
+                        write!(f, "{}", source);
+                    }
+                    None => {}
+                },
+                Err(_) => {
+                    writeln!(
+                        f,
+                        "{}{}",
+                        Color::Yellow.bold().paint("warning"),
+                        Style::new()
+                            .bold()
+                            .paint(": unable to access execution snapshot (are threads locked?)")
+                    );
+                }
+            },
+            None => {}
+        };
+
+        for addl_source in &self.additional_sources {
+            writeln!(f, "{}", addl_source);
+        }
+
+        match &self.note {
+            Some(note) => writeln!(f, "{}: {}", Style::new().bold().paint("note"), note),
+            None => {write!(f, "")},
         }
     }
 }
